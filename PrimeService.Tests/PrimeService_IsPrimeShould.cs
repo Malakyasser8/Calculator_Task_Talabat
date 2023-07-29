88b646b65d35f83415492377f4aaba60beca5193
amir-awad <amirawad004@gmail.com>
2023-07-29 21:19:33 +0300

    Implemented Division and Multiplication with tests

--- PrimeService.Tests/PrimeService_IsPrimeShould.cs
+++ PrimeService.Tests/PrimeService_IsPrimeShould.cs
@@ -69,5 +69,67 @@ namespace Prime.UnitTests.Services
 
         }
 
+        [Test]
+        public void Subtract_InputIsNeg2147483647and2147483647_ReturnException()
+        {
+            Assert.Throws<OverflowException>(() =>
+            {
+                var result = _primeService.Subtract(-2147483647, 2147483647);
+            });
+
+        }
+
+        [Test]
+        public void Divide_InputIs14and2_Return7()
+        {
+            var result = _primeService.Divide(14, 2);
+            Assert.AreEqual(result, 7);
+
+        }
+
+        [Test]
+        public void Divide_InputIs9and4_Return2()
+        {
+            var result = _primeService.Divide(9, 4);
+            Assert.AreEqual(result, 2);
+
+        }
+
+        [Test]
+        public void Divide_InputIs3and0_ThrowsException()
+        {
+            Assert.Throws<DivideByZeroException>(() =>
+            {
+                var result = _primeService.Divide(3, 0);
+            });
+
+        }
+
+        [Test]
+        public void Multiply_InputIs9and4_Return36()
+        {
+            var result = _primeService.Multiply(9, 4);
+            Assert.AreEqual(result, 36);
+
+        }
+
+        [Test]
+        public void Multiply_InputIsNeg7and5_ReturnNeg35()
+        {
+            var result = _primeService.Multiply(-7, 5);
+            Assert.AreEqual(result, -35);
+
+        }
+
+        [Test]
+        public void Multiply_InputIs2147483647and2147483647_ThrowsOverflowException()
+        {
+
+            Assert.Throws<OverflowException>(() =>
+            {
+                var result = _primeService.Multiply(2147483647, 2147483647);
+            });
+        }
+
     }
 }
